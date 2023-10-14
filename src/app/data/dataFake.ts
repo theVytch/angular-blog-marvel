203e80316e6e21f9826bac2f6409543939e24cef
TheVytch <edu.vytch@gmail.com>
2023-10-13 22:08:00 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do pantera negra lançado em breve",
+    "description": "bla blabla",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "bla blabla",
+    "photoCover":"https://files.tecnoblog.net/wp-content/uploads/2021/04/x-men-ordem-2-700x394.jpeg"
+  }
+]
